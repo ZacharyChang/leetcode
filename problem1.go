91f77afa6e9d31fd4a82044f65f4a32249bc6a69
Zachary <zacharychang.zc@gmail.com>
2018-03-12 19:39:43 +0800

    solve: add problem 1.

---
+++ problem1.go
@@ -0,0 +1,25 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	i := 0
+	j := 1
+	for ; i < len(nums); i++ {
+		for j = i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{i, j}
+}
