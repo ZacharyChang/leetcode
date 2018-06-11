4a58dca82679f2b82e3ccb27b18c2c46eee81ce7
zachary <zacharychang.zc@gmail.com>
2018-06-11 20:58:40 +0800

    solve: add #217

---
+++ 217.contains-duplicate/solution_test.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import "testing"
+
+func Test_containsDuplicate(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"[Test Case 1]",
+			args{
+				[]int{1, 2, 3, 1},
+			},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := containsDuplicate(tt.args.nums); got != tt.want {
+				t.Errorf("containsDuplicate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
