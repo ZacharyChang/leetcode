9ba1ffe99c2482cdcf36a424207a4d48a06a5034
Zachary <zacharychang.zc@gmail.com>
2018-08-23 20:31:44 +0800

    test: update test cases

--- 153.find-minimum-in-rotated-sorted-array/solution_test.go
+++ 153.find-minimum-in-rotated-sorted-array/solution_test.go
@@ -30,13 +30,20 @@ func Test_findMin(t *testing.T) {
 			1,
 		},
 		{
-			"[Test Case 2]",
+			"[Test Case 3]",
 			args{
 				[]int{
 					1, 2, 3,
 				},
 			},
 			1,
+		},
+		{
+			"[Test Case 4]",
+			args{
+				[]int{},
+			},
+			0,
 		},
 	}
 	for _, tt := range tests {
