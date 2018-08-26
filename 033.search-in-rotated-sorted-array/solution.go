384e1ef982572a512d61397e8db0a1b4e7fed0a2
Zachary <zacharychang.zc@gmail.com>
2018-08-26 19:41:18 +0800

    impv: update #033

--- 033.search-in-rotated-sorted-array/solution.go
+++ 033.search-in-rotated-sorted-array/solution.go
@@ -5,39 +5,40 @@ func search(nums []int, target int) int {
 	if nums == nil || len(nums) == 0 {
 		return -1
 	}
-	if nums[0] == target {
-		return 0
+	start, end := 0, len(nums)-1
+	if nums[start] == target {
+		return start
 	}
-	if nums[len(nums)-1] == target {
-		return len(nums) - 1
+	if nums[end] == target {
+		return end
 	}
-	start := 0
-	end := len(nums) - 1
 	for start < end-1 {
 		mid := (start + end) / 2
 		if nums[mid] == target {
 			return mid
 		}
-		// pivot在nums[mid:](右半边)中，左半边为顺序递增
-		if nums[mid] > nums[end] {
-			if target > nums[start] && target < nums[mid] {
-				end = mid
-			} else {
-				start = mid
-			}
-		} else if nums[mid] < nums[start] { // pivot在nums[:mid](左半边)中，右半边为顺序递增
-			if target > nums[mid] && target < nums[end] {
+		// 因为不重复，所以nums[mid]!=nums[start]
+		if nums[mid] < nums[start] { // pivot在nums[:mid](左半边)中，右半边为顺序递增
+			if between(target, nums[mid], nums[end]) {
 				start = mid
 			} else {
 				end = mid
 			}
-		} else { // pivot不在nums[start:end]中
-			if nums[mid] > target {
+		} else if nums[mid] > nums[start] { // pivot在nums[mid:](右半边)中，左半边为顺序递增
+			if between(target, nums[start], nums[mid]) {
 				end = mid
 			} else {
 				start = mid
 			}
 		}
+
 	}
 	return -1
+}
+
+func between(a int, b int, c int) bool {
+	if a > b && a < c {
+		return true
+	}
+	return false
 }
