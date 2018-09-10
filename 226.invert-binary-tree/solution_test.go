0ce9cd4843b58b223c3c4483c14e89980da4df6c
Zachary <zacharychang.zc@gmail.com>
2018-09-10 18:13:24 +0800

    fix: update test cases

--- 226.invert-binary-tree/solution_test.go
+++ 226.invert-binary-tree/solution_test.go
@@ -1,6 +1,9 @@
 package leetcode
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_invertTree(t *testing.T) {
 	type args struct {
@@ -60,7 +63,7 @@ func Test_invertTree(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := invertTree(tt.args.root); got != tt.want {
+			if got := invertTree(tt.args.root); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("invertTree() = %v, want %v", got, tt.want)
 			}
 		})
