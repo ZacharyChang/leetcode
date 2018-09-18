359b09b199b1961d9c1ee9c422f0b51cc3e69c5e
Zachary <zacharychang.zc@gmail.com>
2018-09-18 19:43:45 +0800

    solve: #104

---
+++ 104.maximum-depth-of-binary-tree/solution_test.go
@@ -0,0 +1,49 @@
+package leetcode
+
+import "testing"
+
+func Test_maxDepth(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"[Test Case 1]",
+			args{
+				&TreeNode{
+					3,
+					&TreeNode{
+						9,
+						nil,
+						nil,
+					},
+					&TreeNode{
+						20,
+						&TreeNode{
+							15,
+							nil,
+							nil,
+						},
+						&TreeNode{
+							7,
+							nil,
+							nil,
+						},
+					},
+				},
+			},
+			3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDepth(tt.args.root); got != tt.want {
+				t.Errorf("maxDepth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
