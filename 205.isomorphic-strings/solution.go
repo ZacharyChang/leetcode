88a99d78d8b13c19742eaa67f97c6178f0200ec4
Zachary <zacharychang.zc@gmail.com>
2018-10-15 00:12:50 +0800

    improve: use array instead of map

--- 205.isomorphic-strings/solution.go
+++ 205.isomorphic-strings/solution.go
@@ -1,11 +1,12 @@
 package leetcode
 
 func isIsomorphic(s string, t string) bool {
-	stMap := make(map[rune]rune, len(s))
-	tsMap := make(map[rune]rune, len(s))
+	// map[byte]byte can be replaced by array
+	stMap := make([]byte, 256)
+	tsMap := make([]byte, 256)
 
 	for i := 0; i < len(s); i++ {
-		sVal, tVal := rune(s[i]), rune(t[i])
+		sVal, tVal := s[i], t[i]
 		if stMap[sVal] > 0 && stMap[sVal] != tVal {
 			return false
 		}
--- 205.isomorphic-strings/solution_test.go
+++ 205.isomorphic-strings/solution_test.go
@@ -36,14 +36,6 @@ func Test_isIsomorphic(t *testing.T) {
 			},
 			false,
 		},
-		// {
-		// 	"[Test Case 3]",
-		// 	args{
-		// 		"foo",
-		// 		"ba",
-		// 	},
-		// 	false,
-		// },
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
