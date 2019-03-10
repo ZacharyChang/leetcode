05e28e633708fb7d1dc766961257746ebec35935
Zachary <zacharychang.zc@gmail.com>
2019-03-10 22:07:28 +0800

    impv: #720 space complexity

--- 720.longest-word-in-dictionary/solution.go
+++ 720.longest-word-in-dictionary/solution.go
@@ -8,12 +8,10 @@ func longestWord(words []string) string {
 
 	wordMap := make(map[string]bool, 0)
 	sort.Strings(words)
-	for _, v := range words {
-		wordMap[v] = true
-	}
 	maxLen := 0
 	result := ""
 	for _, word := range words {
+		wordMap[word] = true
 		if len(word) > maxLen && checkValid(word, &wordMap) {
 			result = word
 			maxLen = len(word)
