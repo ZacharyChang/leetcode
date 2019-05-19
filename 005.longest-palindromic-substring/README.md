f0c340259ac2b99533300b63dfe7525278ee48b5
Zachary <zacharychang.zc@gmail.com>
2019-05-19 22:32:55 +0800

    improve: #005 expand around center

---
+++ 005.longest-palindromic-substring/README.md
@@ -0,0 +1,23 @@
+# Longest Palindromic Substring
+-----
+## Description
+Given a string s, find the `longest palindromic substring` in s. You may assume that the maximum length of s is `1000`.
+
+## Example
+### Example 1
+```
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+```
+
+### Example 2
+```
+Input: "cbbd"
+Output: "bb"
+```
+
+## Tags
+**[String](https://leetcode.com/tag/string)**
+
+**[Dynamic Programming](https://leetcode.com/tag/dynamic-programming)**
---
+++ 005.longest-palindromic-substring/solution.go
@@ -0,0 +1,36 @@
+package leetcode
+
+// expand around center
+// time complexity: O(n^2)
+// TODO: DP
+func longestPalindrome(s string) string {
+	if len(s) <= 1 {
+		return s
+	}
+	res := ""
+	max := 0
+	for i := 0; i < len(s)-1; i++ {
+		left, right := findPalindrome(s, i, i)
+		if right-left+1 > max {
+			max = right - left + 1
+			res = s[left : right+1]
+		}
+		left, right = findPalindrome(s, i, i+1)
+		if right-left+1 > max {
+			max = right - left + 1
+			res = s[left : right+1]
+		}
+	}
+	return res
+}
+
+func findPalindrome(s string, left int, right int) (int, int) {
+	if s[left] != s[right] {
+		return 0, 0
+	}
+	for left-1 >= 0 && right+1 < len(s) && s[left-1] == s[right+1] {
+		left--
+		right++
+	}
+	return left, right
+}
