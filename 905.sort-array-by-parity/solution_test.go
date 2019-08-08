b7fea62d5c4d1abd1f052f42f70da0812685a31d
Zachary <zacharychang.zc@gmail.com>
2019-08-08 21:00:49 +0800

    fix: #905 test cases

--- 905.sort-array-by-parity/solution_test.go
+++ 905.sort-array-by-parity/solution_test.go
@@ -12,7 +12,7 @@ func Test_sortArrayByParity(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want []int
+		want [][]int
 	}{
 		{
 			"[Test Case 1]",
@@ -21,16 +21,71 @@ func Test_sortArrayByParity(t *testing.T) {
 					3, 1, 2, 4,
 				},
 			},
-			[]int{
-				2, 4, 3, 1,
+			[][]int{
+				{
+					2, 4, 3, 1,
+				},
+				{
+					4, 2, 3, 1,
+				},
+				{
+					2, 4, 1, 3,
+				},
+				{
+					4, 2, 1, 3,
+				},
+			},
+		},
+		{
+			"[Test Case 2]",
+			args{
+				[]int{
+					1, 2,
+				},
+			},
+			[][]int{
+				{
+					2, 1,
+				},
+			},
+		},
+		{
+			"[Test Case 3]",
+			args{
+				[]int{
+					4, 2, 1, 3,
+				},
+			},
+			[][]int{
+				{
+					2, 4, 3, 1,
+				},
+				{
+					4, 2, 3, 1,
+				},
+				{
+					2, 4, 1, 3,
+				},
+				{
+					4, 2, 1, 3,
+				},
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := sortArrayByParity(tt.args.A); !reflect.DeepEqual(got, tt.want) {
+			if got := sortArrayByParity(tt.args.A); !in(got, tt.want) {
 				t.Errorf("sortArrayByParity() = %v, want %v", got, tt.want)
 			}
 		})
 	}
+}
+
+func in(target []int, results [][]int) bool {
+	for _, res := range results {
+		if reflect.DeepEqual(target, res) {
+			return true
+		}
+	}
+	return false
 }
