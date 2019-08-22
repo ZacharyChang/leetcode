34c04e10b4f5ecb07906dfa1078bf4dc71713f15
Zachary <zacharychang.zc@gmail.com>
2019-08-22 22:31:15 +0800

    improve: #933 by binary search

--- 933.number-of-recent-calls/solution.go
+++ 933.number-of-recent-calls/solution.go
@@ -12,11 +12,28 @@ func Constructor() RecentCounter {
 
 func (this *RecentCounter) Ping(t int) int {
 	this.queue = append(this.queue, t)
-	for i := 0; i < len(this.queue); i++ {
-		if t-this.queue[i] <= 3000 {
-			this.queue = this.queue[i:]
-			break
+	idx := binarySearch(this.queue, t-3000)
+	this.queue = this.queue[idx:]
+	return len(this.queue)
+}
+
+func binarySearch(array []int, target int) int {
+	start, end := 0, len(array)-1
+	if array[start] >= target {
+		return start
+	}
+	if array[end] <= target {
+		return end
+	}
+	for start < end-1 {
+		mid := (start + end) / 2
+		if array[mid] == target {
+			return mid
+		} else if array[mid] > target {
+			end = mid
+		} else {
+			start = mid
 		}
 	}
-	return len(this.queue)
+	return end
 }
