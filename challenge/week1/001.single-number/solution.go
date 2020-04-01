eb583ffd7660f755d1be35d39f5d7928385d9e3e
Zachary <zacharychang.zc@gmail.com>
2020-04-01 21:23:34 +0800

    solve: challenge #001

---
+++ challenge/week1/001.single-number/solution.go
@@ -0,0 +1,9 @@
+package challenge
+
+func singleNumber(nums []int) int {
+	res := 0
+	for _, v := range nums {
+		res ^= v
+	}
+	return res
+}
---
+++ challenge/week1/001.single-number/solution_test.go
@@ -0,0 +1,40 @@
+package challenge
+
+import "testing"
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"[Test Case 1]",
+			args{
+				[]int{
+					2, 2, 1,
+				},
+			},
+			1,
+		},
+		{
+			"[Test Case 2]",
+			args{
+				[]int{
+					4, 1, 2, 1, 2,
+				},
+			},
+			4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
