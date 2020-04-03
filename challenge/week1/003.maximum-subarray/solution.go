3ae79aa6f70d0b4f67c06186f15b90615221f86b
Zachary <zacharychang.zc@gmail.com>
2020-04-03 20:19:30 +0800

    solve: challenge #003

---
+++ challenge/week1/003.maximum-subarray/solution.go
@@ -0,0 +1,22 @@
+package leetcode
+
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	res := dp[0]
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(dp[i-1]+nums[i], nums[i])
+		res = max(res, dp[i])
+	}
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
